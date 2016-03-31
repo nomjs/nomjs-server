b8e6b342bbe2138ebf5432fd43e40b3938242fe4
Sean McIntyre <s.mcintyre@xverba.ca>
2016-03-31 18:05:33 -0400

    Cleaning, commenting.

--- src/modules/packages.js
+++ src/modules/packages.js
@@ -6,14 +6,27 @@ const Ravel = require('ravel');
  * Logic for listing, retrieving, publishing packages
  */
 class Packages extends Ravel.Module {
+
+  isScoped(id) {
+    return id[0] === '@';
+  }
+
+  /**
+   * Retrieve package information based on a package name
+   * @param id {String} the package name (such as @raveljs/ravel).
+   * @return {Promise} resolves if the package is in the nom registry, rejects otherwise
+   */
   info(id) {
     this.log.info(`client asked for info on: ${id}`);
+
     return new Promise((resolve, reject) => {
-      if (id[0] === '@') {
+      // is this a scoped package?
+      if (this.isScoped(id)) {
         reject(new this.ApplicationError.NotFound({
-          error: `nom can't find the package you're looking for. Please encourage the developer to submit it to nom!`
+          error: `nom can't find the package you're looking for.\nPlease encourage the developer to submit it to nom!`
         }));
       } else {
+        // not a scoped package
         reject(new this.ApplicationError.IllegalValue({
           error: 'nom does not permit unscoped packages'
         }));
