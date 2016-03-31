da9770964fb7b04965b3bd76659e1c00080a0a0f
Sean McIntyre <s.mcintyre@xverba.ca>
2016-03-31 18:00:04 -0400

    Issue a different error message when a user asks for an unscoped package.

--- src/modules/packages.js
+++ src/modules/packages.js
@@ -9,9 +9,15 @@ class Packages extends Ravel.Module {
   info(id) {
     this.log.info(`client asked for info on: ${id}`);
     return new Promise((resolve, reject) => {
-      reject(new this.ApplicationError.NotFound({
-        error: 'Not found'
-      }));
+      if (id[0] === '@') {
+        reject(new this.ApplicationError.NotFound({
+          error: `nom can't find the package you're looking for. Please encourage the developer to submit it to nom!`
+        }));
+      } else {
+        reject(new this.ApplicationError.IllegalValue({
+          error: 'nom does not permit unscoped packages'
+        }));
+      }
     });
   }
 }
