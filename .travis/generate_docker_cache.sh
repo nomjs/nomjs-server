a3069392d5a7f55e17f570395d5e16869ae7a6df
Michael Laccetti <michael@laccetti.com>
2016-12-12 13:09:30 -0500

    More Travis twiddling
    * No more node.js 6; require 7
    * Better cache generation

--- .travis/generate_docker_cache.sh
+++ .travis/generate_docker_cache.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-if [ ! -d ".travis/docker" ]
+numfiles=(.travis/docker/*)
+numfiles=${#numfiles[@]}
+
+echo "Number of files: $numfiles"
+
+if [ $numfiles -le 0 ]
 then
-  mkdir .travis/docker
+  echo "Backing up Docker images."
+  mkdir -p .travis/docker
   docker export rethinkdb | gzip -c > .travis/docker/rethinkdb_export.tgz
   docker export redis | gzip -c > .travis/docker/redis.tgz
+else
+  echo "Docker images already backed up, skipping."
 fi
