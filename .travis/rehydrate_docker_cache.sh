1714c884b837be45ba15ad0c8151160113b59067
Michael Laccetti <michael@laccetti.com>
2016-12-12 13:14:26 -0500

    Update the Travis rehydrate script
    Do it by file count, not directory existence

--- .travis/rehydrate_docker_cache.sh
+++ .travis/rehydrate_docker_cache.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-if [ -d ".travis/docker" ]
+numfiles=(.travis/docker/*)
+numfiles=${#numfiles[@]}
+
+echo "Number of files: $numfiles"
+
+if [ $numfiles -ge 0 ]
 then
+  echo "Rehydrating all Docker images."
   for image in `ls .travis/docker`
   do
+    echo "Rehydrating ${image}"
     docker import .travis/docker/${image}
   done
+else
+    echo "No Docker images to rehydrate."
 fi
