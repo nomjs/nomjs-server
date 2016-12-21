94d0835266ac6a052c4fc3b5903ce9b7d1f304fc
Michael Laccetti <michael@laccetti.com>
2016-12-20 20:59:00 -0500

    Getting auth functional
    Wow, the hoops.

--- src/routes/user.js
+++ src/routes/user.js
@@ -26,21 +26,32 @@ class UserRoutes extends Routes {
   async putHandler (ctx) {
     this.log.info('Logging in a user.');
 
+    if (!ctx.request.fields.name) {
+      return new Promise((resolve, reject) => {
+        this.log.warn('No "name" field provided for authentication, faling.');
+        ctx.status = Ravel.httpCodes.INTERNAL_SERVER_ERROR;
+        reject('No "name" field provided for authentication.');
+      });
+    }
+
     let promise, token;
     // we use the special username 'oauth' to switch into token mode (for 2fa users)
     if (ctx.request.fields.name === 'oauth') {
+      this.log.info('User is invoking oauth authentication.');
       // if in token mode, verify token has the correct scopes
       promise = this.github.verifyToken(
         ctx.request.fields.password
       );
     } else {
       // if in username/password mode, generate a token for nom with the correct scopes
+      this.log.info('User is signing in with their username and password.');
       promise = this.github.generateToken(
         ctx.request.fields.name,
         ctx.request.fields.password
       );
     }
     // now get the user profile
+    this.log.info('Retrieving user profile.');
     return promise.then((t) => {
       token = t;
       return this.github.getProfile(t);
@@ -49,10 +60,12 @@ class UserRoutes extends Routes {
     // We don't store the token credential.
     // Web application flow will be used to get a token for nom web UI: https://developer.github.com/v3/oauth/#web-application-flow
       .then((profile) => {
+        this.log.debug('Creating user in nomjs-registry.');
         return this.users.findOrCreateUser(profile, ctx.request.fields.email);
       })
       // respond
       .then(() => {
+        this.log.debug('User authenticated and signed in, returning information.');
         ctx.status = Ravel.httpCodes.CREATED;
         ctx.response.body = {
           ok: true,
@@ -60,7 +73,8 @@ class UserRoutes extends Routes {
           'token': token
         };
       }).catch((err) => {
-        this.log.error(err.stack);
+        this.log.error('Could not sign in user.', err);
+        ctx.status = Ravel.httpCodes.INTERNAL_SERVER_ERROR;
       });
   }
 }
